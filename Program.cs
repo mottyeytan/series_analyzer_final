55ea68b1013a748c27956edc74b95b2f14b69ad7
שם המשתמש שלך <your-email@example.com>
2025-05-12 16:27:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace the_series_anylzer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+                 {
+            
+            foreach (var arg in args)
+            {
+                Console.WriteLine("hello");
+                Console.WriteLine(arg);
+            }
+            {
+                
+            }
+        }
+    }
+}
