1900819dbfb91a0889b4e76943ff6a4917a9629d
שם המשתמש שלך <your-email@example.com>
2025-05-13 10:15:52 +0300

    add th function for reciving numbers

--- Program.cs
+++ Program.cs
@@ -2,18 +2,44 @@
 {
     class Program
     {
+        
+        List<int> series = new List<int>();
+        static int length = 0;
+
+        static List<string> ReciveListOfNums(string[] args)
+        {
+            List<string> list = new List<string>();
+            if (args.Length >= 3)
+            {
+                list = new List<string>(args);
+            }
+            else
+            {
+                string input = null;
+                while (input != "-1")
+                {
+                    Console.WriteLine("please enter a number" +
+                                      "for exit enter -1");
+                    input = Console.ReadLine();
+                    if (input == "-1") break;
+                    list.Add(input);
+                }
+            }
+            return list;
+        }
+        
+        
+        
 
         static void Main(string[] args)
-                 {
+        {
+           recivelistofnums(args);
+            }
             
-            foreach (var arg in args)
-            {
-                Console.WriteLine("hello");
-                Console.WriteLine(arg);
-            }
-            {
+        
+                 
                 
-            }
-        }
+           
+       
     }
 }
